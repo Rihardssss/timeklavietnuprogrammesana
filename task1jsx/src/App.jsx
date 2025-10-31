e59a4b9c784997c71fedb0d51b3847ddc488e2f8
Rihardssss <154497544+Rihardssss@users.noreply.github.com>
2025-10-31 08:57:45 +0200

    miks ir zars

---
+++ task1jsx/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <img src="/ICEGERGERT.jpg" alt="Profile" />
+      <div className="card-info">
+        <h1>Rihards Indārs</h1>
+        <h2>git start learner</h2>
+        <p>Passionate about starting GIT</p>
+        <a
+          href="https://www.atlassian.com/git/glossary#commands"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Iemacities laist git start
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
